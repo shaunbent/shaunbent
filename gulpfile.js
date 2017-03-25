2622f0c8e69344314d5e072b7581ae89edc73053
Shaun Bent <shaunb@spotify.com>
2017-03-25 17:04:57 +0000

    Boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const gulp = require('gulp');
+const del = require('del');
+const runSequence = require('run-sequence');
+const browserSync = require('browser-sync');
+const gulpLoadPlugins = require('gulp-load-plugins');
+
+// load the plugins into an array of plugins
+const $ = gulpLoadPlugins();
+
+// configs
+const sassOptions = { outputStyle: 'nested' };
+const supportedBrowsers = ['IE 9', 'last 2 versions', '> 1%', 'Safari 7'];
+const autoprefixerOptions = { browsers: supportedBrowsers };
+const nanoOptions = {
+  autoprefixer: { browsers: supportedBrowsers },
+  convertValues: { length: false, boolean: false },
+  mergeRules: false
+};
+
+// tasks
+gulp.task('assets', () =>
+  gulp
+    .src([
+      'assets/**/*.{woff,woff2,txt,jpg,png,gif,svg,md}'
+    ])
+    .pipe(gulp.dest('dist/assets/'))
+);
+
+gulp.task('styles', () =>
+  gulp
+    .src('app/styles/*.scss')
+    .pipe(
+      $.sass(sassOptions)
+      .on('error', $.sass.logError)
+    )
+    .pipe($.autoprefixer(autoprefixerOptions))
+    .pipe($.cssnano(nanoOptions))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(browserSync.stream({ match: '**/*.css' }))
+);
+
+gulp.task('views', () =>
+  gulp
+    .src([
+      'app/views/**/*.html',
+      '!app/views/**/_*.html'
+    ], { base: 'app/views' })
+    .pipe($.nunjucks.compile())
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('clean', () => del(['dist'], { dot: true }) );
+
+gulp.task('default', ['build'], () => {
+  browserSync({
+    notify: false,
+    server: 'dist',
+    online: true
+  });
+
+  gulp.watch('app/styles/*.scss', ['styles']);
+  gulp.watch('app/views/**/*.html', ['views']);
+  gulp.watch('assets/**/*.{woff,woff2,txt,jpg,png,gif,svg,md}', ['assets']);
+  gulp.watch([
+    'dist/**/*.html',
+    'dist/assets/*.{woff,woff2,txt,jpg,png,gif,svg,md}',
+    'dist/assets/styles/*.css'
+  ]).on('change', browserSync.reload);
+});
+
+gulp.task('build', callback => {
+  runSequence('clean', 'assets', 'views', 'styles', callback);
+});
