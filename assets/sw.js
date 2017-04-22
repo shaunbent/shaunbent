abf4f5babc21f4e016a156ec756c6d0238224957
Shaun Bent <shaunb@spotify.com>
2017-04-22 16:23:57 +0200

    AB SW

--- assets/sw.js
+++ assets/sw.js
@@ -1,51 +1,49 @@
-const CACHE_NAME = 'shaunbent:0001';
-
+const cacheName = 'shaunbent:0001';
 const cacheFiles = [
   '/',
   '/assets/images/pattern.svg'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(cacheFiles);
-      })
-  );
-});
+    event.waitUntil(
+      caches.open(cacheName)
+        .then((cache) => {
+          return cache.addAll(cacheFiles)
+            .then(() => self.skipWaiting());
+        })
+    );
+  }
+);
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request);
-    }).catch(() => {
-      console.log('Fetch failed; returning offline page instead.', error);
-
-      // return caches.match('/offline/');
-    })
-  );
-});
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.match(event.request)
+          .then(response => {
+            return response || fetch(event.request);
+          }
+        );
+      })
+    );
+  }
+);
 
 // Empty out any caches that don’t match the ones listed.
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['shaunbent:0001'];
-
-  event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      }
-    )
-  );
-});
+// self.addEventListener('activate', (event) => {
+//   const cacheWhitelist = ['shaunbent:0001'];
+// 
+//   event.waitUntil(
+//     caches.keys()
+//       .then((cacheNames) => {
+//         return Promise.all(
+//           cacheNames.map((cacheName) => {
+//             if (cacheWhitelist.indexOf(cacheName) === -1) {
+//               return caches.delete(cacheName);
+//             }
+//           })
+//         );
+//       }
+//     )
+//   );
+// });
